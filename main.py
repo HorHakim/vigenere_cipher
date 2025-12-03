d66b0d0069bed8e0cc3273c45b77c1d099b06f6a
Hakim <horairyhakim@gmail.com>
2025-12-03 14:51:19 +0100

    feature: adding cesar brute force algorithm

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 def cesar_cipher(text, key, cipher=True):
 
 	key = key if cipher else -key # ternary
@@ -10,12 +12,25 @@ def cesar_cipher(text, key, cipher=True):
 	return crypted_text
 
 
+def brute_force_cesar_cipher(crypted_text):
+	for potential_key in range(1, 1_114_112):
+		potential_initial_text = cesar_cipher(crypted_text, potential_key, cipher=False)
+
+		for char in potential_initial_text:
+			if char in string.printable:
+				print(potential_key)
+				print(potential_initial_text)
+				print("-----")
+				break
 
 
 
 
-crypted_text = cesar_cipher(text="lapin", key=3_000_000)
+crypted_text = cesar_cipher(text="lapin", key=554)
 print(crypted_text)
 
-initial_text = cesar_cipher(crypted_text, 3_000_000, False)
-print(initial_text)
+# initial_text = cesar_cipher(text=crypted_text, key=3_000_000, cipher=False)
+# print(initial_text)
+
+
+brute_force_cesar_cipher(crypted_text)
